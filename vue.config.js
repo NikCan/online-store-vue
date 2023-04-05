7f5fb2738f7176d4dc8c76623213c33415b5b302
Nikita Gaponov <nikitagaponov@gmail.com>
2023-04-05 17:04:22 +0300

    feat: gh-pages

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/online-store-vue/'
-    : ''
+  publicPath: '/online-store-vue/'
 })
